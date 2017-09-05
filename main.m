2ff64f38bd97de8482a2348de9ae753c2f22429b
David <dk.shin1992@gmail.com>
2017-09-05 17:36:29 +1000

    single coil at any orientation

--- main.m
+++ main.m
@@ -9,8 +9,8 @@ mu_0=4*pi*1e-7;     % vacuum permeability [Tm/A]
 % config
 R=10e-3;    % coil radius [m]
 I=1;        % coil current [A]
-pos=[5e-3,5e-3,0];      % coil centre position (x,y,z)
-ori=[0,0];            % coil orientation (azim,polar)
+pos=[0,0,-10e-3];      % coil centre position (x,y,z)
+ori=[0,0,0];           % coil orientation - Euler angles
 
 % region to evaluate
 nzz=20;
@@ -19,7 +19,6 @@ ntheta=20;
 zz=linspace(-20e-3,20e-3,nzz);    % axial [m]
 rr=linspace(0,20e-3,nrr);        % radial (cylinder) [m]
 theta=linspace(0,2*pi,ntheta);  % azim angle [rad]
-% [RR,ZZ]=meshgrid(rr,zz);
 [TT,RR,ZZ]=meshgrid(theta,rr,zz);
 
 % evaluate the analytic magnetic field solution
@@ -33,17 +32,23 @@ Brr(~isfinite(Brr))=NaN;
 
 Babs=sqrt(Bzz.^2+Brr.^2);     % absolute magnetic field strength [T]
 
+% evaluate in Cartesian coordinate system
+[xx,yy,zz]=pol2cart(TT,RR,ZZ);
+[Bxx,Byy,Bzz]=pol2cart(TT,Brr,Bzz);
+
+% coil orientation + position transform
+Mrot=euler2rotm(ori);
+[xx_tf,yy_tf,zz_tf]=rotmesh(Mrot,xx,yy,zz);
+[Bxx_tf,Byy_tf,Bzz_tf]=rotmesh(Mrot,Bxx,Byy,Bzz);
+
+xx_tf=xx_tf+pos(1);
+yy_tf=yy_tf+pos(2);
+zz_tf=zz_tf+pos(3);
+
 % plot the magnetic field
 % quiver plot for B field
-[xx,yy,zz]=pol2cart(TT,RR,ZZ);
-
-[Bxx,Byy,Bzz]=pol2cart(TT,Brr,Bzz);
-% Bxx(~isfinite(Bxx))=NaN;
-% Byy(~isfinite(Byy))=NaN;
-% Bzz(~isfinite(Bzz))=NaN;
-
 figure();
-quiver3(xx,yy,zz,Bxx,Byy,Bzz,...
+quiver3(xx_tf,yy_tf,zz_tf,Bxx_tf,Byy_tf,Bzz_tf,...
     'Color','k','LineWidth',1.5,'Visible','off');
 hold on;
 
@@ -54,7 +59,7 @@ Bisoval=linspace(Bmin,Bmax,nBisosurf);
 cc=viridis(nBisosurf);
 p={};
 for ii=1:nBisosurf
-    p{ii}=isosurface(xx,yy,zz,Babs,Bisoval(ii));
+    p{ii}=isosurface(xx_tf,yy_tf,zz_tf,Babs,Bisoval(ii));
     patch(p{ii},'FaceColor',cc(ii,:),'EdgeColor','none','FaceAlpha',0.15);
 end
 daspect([1,1,1]);
@@ -68,3 +73,13 @@ ylabel('Y');
 zlabel('Z');
 
 % Babs contours (potential landscape)
+
+%% functions
+% useful rotator for meshgrid
+function [x,y,z]=rotmesh(Mrot,x0,y0,z0)
+nn=size(x0);    % size of the meshgrid
+xyz=Mrot*[x0(:)';y0(:)';z0(:)'];
+x=reshape(xyz(1,:),nn);
+y=reshape(xyz(2,:),nn);
+z=reshape(xyz(3,:),nn);
+end
