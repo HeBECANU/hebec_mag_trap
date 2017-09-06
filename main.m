ceb07424c688da4b45205cf4418d2fe52e8cce9b
David <dk.shin1992@gmail.com>
2017-09-06 10:26:24 +1000

    tidy code

--- main.m
+++ main.m
@@ -4,16 +4,16 @@
 
 t_start=tic;
 
-% physical constants
-mu_0=4*pi*1e-7;     % vacuum permeability [Tm/A]
-
 % TODO:
 % [x] reverse transform mesghrid [xx,yy,zz] in trap coord to evaluate 
 %   similarly ordered (TT,RR,ZZ) vectors for each unique coil
-% [] evaluate B from each coil in trap coord
-% [] sum B - total B field
+% [x] evaluate B from each coil in trap coord
+% [x] sum B - total B field
+% [] set up to biQUIC dimension - Ross
+% [] Voltage to current conversion
 % [] characterise potential
-%   [] isosurfaces on B_tot
+%   [] isosurfaces on B magnitude
+%   [] trap frequency
 
 %% Config grid
 ngrid=100;          % 50 - med; 300 - very fine;
@@ -48,21 +48,80 @@ XYZ0=cell(3,1);
 % end
 % 
 % [Bxx,Byy,Bzz]=Bfield_coil(R_coil,I_coil,XYZ0_coil{:});
-% Babs=sqrt(Bxx.^2+Byy.^2+Bzz.^2);     % absolute magnetic field strength [T]
+% Bmag=sqrt(Bxx.^2+Byy.^2+Bzz.^2);     % absolute magnetic field strength [T]
 
 %% Scenario 2: anti-Helmholtz with 2 coils
+% %%% config
+% % coil 1
+% R_coil{1}=10e-3;    % coil radius [m]
+% I_coil{1}=-1;        % coil current [A]
+% ori_coil{1}=[0,0,0];           % coil orientation - Euler angles
+% pos_coil{1}=[0,0,-4e-3];           % coil centre position (x,y,z)
+% 
+% % coil 2
+% R_coil{2}=10e-3;    % coil radius [m]
+% I_coil{2}=1;        % coil current [A]
+% ori_coil{2}=[0,0,0];           % coil orientation - Euler angles
+% pos_coil{2}=[0,0,4e-3];           % coil centre position (x,y,z)
+% 
+% %%% evaluate the grid region in coil centered frame
+% for ii=1:length(R_coil)
+%     R_coil_this=R_coil{ii};
+%     I_coil_this=I_coil{ii};
+%     pos_coil_this=pos_coil{ii};
+%     
+%     % coord translation
+%     XYZ0_coil_this=cell(3,1);
+%     for jj=1:3
+%         XYZ0_coil_this{jj}=XYZ0{jj}-pos_coil_this(jj);
+%     end
+%     
+%     Bxyz_this=cell(3,1);
+%     [Bxyz_this{1},Bxyz_this{2},Bxyz_this{3}]=Bfield_coil(R_coil_this,I_coil_this,XYZ0_coil_this{:});
+%     
+%     if ii==1
+%         Bxyz=Bxyz_this;
+%     else
+%         Bxyz=cellfun(@(x,y)x+y,Bxyz,Bxyz_this,'UniformOutput',false);
+%     end
+% end
+% Bxx=Bxyz{1};
+% Byy=Bxyz{2};
+% Bzz=Bxyz{3};
+% 
+% Bmag=sqrt(Bxx.^2+Byy.^2+Bzz.^2);     % absolute magnetic field strength [T]
+
+%% Scenario 3: BiQUIC
 %%% config
-% coil 1
-R_coil{1}=10e-3;    % coil radius [m]
-I_coil{1}=-1;        % coil current [A]
+Rquad=5e-3;
+Iquad=1;
+Rshunt=3e-3;
+Ishunt=1;
+
+% QUAD coil 1
+R_coil{1}=Rquad;    % coil radius [m]
+I_coil{1}=-Iquad;        % coil current [A]
 ori_coil{1}=[0,0,0];           % coil orientation - Euler angles
 pos_coil{1}=[0,0,-4e-3];           % coil centre position (x,y,z)
 
-% coil 2
-R_coil{2}=10e-3;    % coil radius [m]
-I_coil{2}=1;        % coil current [A]
+% Quad coil 2
+R_coil{2}=Rquad;    % coil radius [m]
+I_coil{2}=Iquad;        % coil current [A]
 ori_coil{2}=[0,0,0];           % coil orientation - Euler angles
 pos_coil{2}=[0,0,4e-3];           % coil centre position (x,y,z)
+
+% Shunt
+% Shunt coil 1
+R_coil{3}=Rshunt;    % coil radius [m]
+I_coil{3}=-Ishunt;        % coil current [A]
+ori_coil{3}=[0,0,0];           % coil orientation - Euler angles
+pos_coil{3}=[10e-3,0,-4e-3];           % coil centre position (x,y,z)
+
+% Shunt coil 2
+R_coil{4}=Rshunt;    % coil radius [m]
+I_coil{4}=Ishunt;        % coil current [A]
+ori_coil{4}=[0,0,0];           % coil orientation - Euler angles
+pos_coil{4}=[10e-3,0,4e-3];           % coil centre position (x,y,z)
 
 %%% evaluate the grid region in coil centered frame
 for ii=1:length(R_coil)
@@ -73,7 +132,7 @@ for ii=1:length(R_coil)
     % coord translation
     XYZ0_coil_this=cell(3,1);
     for jj=1:3
-        XYZ0_coil_this{jj}=XYZ0{jj}+pos_coil_this(jj);
+        XYZ0_coil_this{jj}=XYZ0{jj}-pos_coil_this(jj);
     end
     
     Bxyz_this=cell(3,1);
@@ -89,9 +148,7 @@ Bxx=Bxyz{1};
 Byy=Bxyz{2};
 Bzz=Bxyz{3};
 
-Babs=sqrt(Bxx.^2+Byy.^2+Bzz.^2);     % absolute magnetic field strength [T]
-
-
+Bmag=sqrt(Bxx.^2+Byy.^2+Bzz.^2);     % absolute magnetic field strength [T]
 
 %% Plot
 % config
@@ -105,8 +162,8 @@ hfig_btrap=figure();
 hold on;
 
 %%% B field magnitude - isosurfaces (isopotentials)
-Bmax=max(Babs(isfinite(Babs)));
-Bmin=min(Babs(isfinite(Babs)));
+Bmax=max(Bmag(isfinite(Bmag)));
+Bmin=min(Bmag(isfinite(Bmag)));
 % Bisoval=linspace(Bmin,Bmax,nBisosurf+2);
 Bisoval=logspace(log10(Bmin),log10(Bmax),nBisosurf+2);
 Bisoval=Bisoval(2:end-1);   % cull the min and max
@@ -114,9 +171,9 @@ cc=viridis(nBisosurf);
 p={};
 pp=[];
 for ii=1:nBisosurf
-    p{ii}=isosurface(1e3*XYZ0{1},1e3*XYZ0{2},1e3*XYZ0{3},Babs,Bisoval(ii));
+    p{ii}=isosurface(1e3*XYZ0{1},1e3*XYZ0{2},1e3*XYZ0{3},Bmag,Bisoval(ii));
     pp(ii)=patch(p{ii},'FaceColor',cc(ii,:),'EdgeColor','none','FaceAlpha',0.15,...
-        'DisplayName',sprintf('%0.2e',1e4*Bisoval(ii)));
+        'DisplayName',sprintf('%0.1g',1e4*Bisoval(ii)));
 end
 box on;
 daspect([1,1,1]);
@@ -128,7 +185,7 @@ camlight;
 lighting gouraud;
 
 lgd=legend(pp(:));
-title(lgd,'B-isosurface (G)');
+title(lgd,'$B$-isosurface (G)');
 
 xlabel('X [mm]');
 ylabel('Y [mm]');
