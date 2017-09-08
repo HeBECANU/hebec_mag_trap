e009342c132d4fa1372843e43ef8d64532dddbe4
David <dk.shin1992@gmail.com>
2017-09-08 10:23:03 +1000

    evaluate trap frequency - arb params

--- main.m
+++ main.m
@@ -151,9 +151,8 @@ btrap=[btrap,bias];
 %% Trap visualisation - B-isosurfaces
 hfig_btrap=plot_B_3d(btrap,Bmag,XYZ);
 
-%% Characterise trap: trap center and frequency
+%% Trap center
 % trap center: point of minimum B magnitude
-% frequency: omega=sqrt(V''/m) [spatial 2nd order derivative]
 
 %%% Find trap center with current trap config
 % find X to minimise $Bmag$ from trap_eval(btrap,X,0,0) - from symmetry
@@ -186,7 +185,7 @@ XYZ_trap=cellfun(@(YXZ) permute(YXZ,[2,1,3]),XYZ_trap,'UniformOutput',false);
 % calculate magnetic field
 [Bmag_trap,Bxyz_trap]=trap_eval(btrap,XYZ_trap{:});
 
-%%% B profile in X,Y,Z line profile
+%%% 1D Bmag profile - X,Y,Z line profile
 % indices to trap centre (in grid)
 [~,I0]=min(abs(Bmag_trap(:)-B_cent));
 II0=zeros(1,3);        % this is ordered in YXY
@@ -196,7 +195,7 @@ B_trap_1d=cell(3,1);     % 1D line-profile of magnetic field potential [T]
 idxcirc=[1,2,3];
 Bmagcirc=Bmag_trap;      % temporary copy
 for ii=1:3
-    B_trap_1d{ii}=Bmagcirc(:,II0(idxcirc(2)),II0(idxcirc(3)));
+    B_trap_1d{ii}=Bmagcirc(:,II0(idxcirc(2)),II0(idxcirc(3)))';     % 1xNi array 
     idxcirc=circshift(idxcirc,-1);
     Bmagcirc=permute(Bmagcirc,[2,3,1]);     % dimension circular permutation
 end
@@ -216,10 +215,12 @@ hfig_bmag_1d=figure();
 axisstr={'X','Y','Z'};
 linestyle={'-','--',':'};
 p=[];
+xyz_trap0=cell(3,1);
 for ii=1:3
     hold on;
     % shift coord to approximate trap center
-    p(ii)=plot(1e3*(xyz_trap{ii}-trap_cent(ii)),1e4*B_trap_1d{ii},...
+    xyz_trap0{ii}=xyz_trap{ii}-trap_cent(ii);       % trap center ref'd disp [m]
+    p(ii)=plot(1e3*xyz_trap0{ii},1e4*B_trap_1d{ii},...
         'LineStyle',linestyle{ii},'LineWidth',1.5,...
         'DisplayName',axisstr{ii});
 end
@@ -228,6 +229,43 @@ xlabel('Displacement [mm]');
 ylabel('$B$ [G]');
 lgd=legend(p);
 title(lgd,sprintf('(%0.2g,%0.2g,%0.2g) [mm]',1e3*trap_cent(:)));
+
+%% Evaluate magnetic trap potential (1d: x-y-z)
+% TODO - U_B± \propto mu_He * (±)Bmag (for weak(strong)-field seeking state)
+const_prop=1;       % TODO
+U_B_1d=cellfun(@(x) const_prop*x,B_trap_1d,'UniformOutput',false);
+
+
+%% Trap frequency
+% frequency: omega=sqrt(U''/m) [spatial 2nd order derivative]
+% trap 1D: B_trap_1d - 3x1 cell array of 1D B magnitude profiles
+
+% numerical second order differential - TODO check
+d2U=cellfun(@(x)diff(x,2),U_B_1d,'UniformOutput',false);    % 2nd order DIFFERENCE in potential
+dx=cellfun(@(x)diff(x),xyz_trap0,'UniformOutput',false);    % 1st ord DIFF in displacement
+
+% evaluate trap frequency (harmonic)
+m_He=1;     % TODO
+f_trap=cellfun(@(D2Y,DX)(2*pi)*sqrt((D2Y./(DX(1:end-1).^2))/m_He),d2U,dx,'UniformOutput',false);     % trap frequency in [Hz]
+
+f0=cellfun(@(x)mean(x),f_trap);     % mean trap frequency around trap centre [Hz]
+
+%%% Visualise trap frequency - anharmonicity, etc
+hfig_trap_freq=figure();
+axisstr={'X','Y','Z'};
+linestyle={'-','--',':'};
+p=[];
+for ii=1:3
+    hold on;
+    p(ii)=plot(1e3*xyz_trap0{ii}(2:end-1),f_trap{ii},...
+        'LineStyle',linestyle{ii},'LineWidth',1.5,...
+        'DisplayName',axisstr{ii});
+end
+box on;
+xlabel('Displacement [mm]');
+ylabel('$f$ [Hz]');
+lgd=legend(p);
+
 
 %% End of code
 t_end=toc(t_start);
