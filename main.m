90254dc625b4c54366156c639fdaf36e83cb348c
David <dk.shin1992@gmail.com>
2017-09-09 22:03:47 +1000

    update main - use biquic trap generator

--- main.m
+++ main.m
@@ -91,64 +91,70 @@ XYZ=cellfun(@(YXZ) permute(YXZ,[2,1,3]),XYZ,'UniformOutput',false);
 % [Bmag,Bxyz]=trap_eval(btrap,XYZ{:});
 
 %% Scenario 3: BiQUIC
-%%% config
-% Quad: 14mm Dia; 10 turns;
-% Shunt (Ioffe): 14mm; 18 turns; 
-% pitch=500um (wire dia); AH sep ~17mm; Q-Sh sep=18.5 mm; ~36 amp (max)
-Dquad=14e-3;
-Dshunt=14e-3;
-
-Nturnshunt=18;
-Nturnquad=10;
-pitch_coil=0.5e-3;
-
-disp_ah=17e-3;
-disp_qs=18.5e-3;
-
 Iquad=1;
 Ishunt=1.5;
-
-Rquad=Dquad/2;
-Rshunt=Dshunt/2;
-
-% Trap bias (nuller) [http://dx.doi.org/10.1063/1.2472600]
 Bbias=1e-4*[1,0,0];     % external bias field [T] (uniform assumption)
 
-%%% Build trap
-% Quadrupole - ref
-quad_coil.type='coil';
-quad_coil.param={Rquad,Iquad,[0,0,disp_ah/2],[0,0,0]};
-% Shunt (Ioffe) - ref
-shunt_coil.type='coil';
-shunt_coil.param={Rshunt,Ishunt,[disp_qs,0,disp_ah/2],[0,0,0]};
-% Bias (nuller)
-bias.type='uniform';
-bias.param={Bbias};
+btrap=biquic_trap(Iquad,Ishunt,Bbias);
 
-btrap=[];
-for ii=1:Nturnquad
-    quad_coil_temp=quad_coil;
-    % shift by wire pitch
-    quad_coil_temp.param{3}=quad_coil_temp.param{3}+(ii-1)*[0,0,pitch_coil];
-    btrap=[btrap,quad_coil_temp];
-    
-    % anti-helmholtz pair - mirror symmetry around Z
-    quad_coil_temp.param{2}=-1*quad_coil_temp.param{2};     % flip current dir
-    quad_coil_temp.param{3}=[1,1,-1].*quad_coil_temp.param{3};
-    btrap=[btrap,quad_coil_temp];
-end
-for ii=1:Nturnshunt
-    shunt_coil_temp=shunt_coil;
-    % shift by wire pitch
-    shunt_coil_temp.param{3}=shunt_coil_temp.param{3}+(ii-1)*[0,0,pitch_coil];
-    btrap=[btrap,shunt_coil_temp];
-    
-    % anti-helmholtz pair - mirror symmetry around Z
-    shunt_coil_temp.param{2}=-1*shunt_coil_temp.param{2};   % flip current dir
-    shunt_coil_temp.param{3}=[1,1,-1].*shunt_coil_temp.param{3};
-    btrap=[btrap,shunt_coil_temp];
-end
-btrap=[btrap,bias];
+% %%% config
+% % Quad: 14mm Dia; 10 turns;
+% % Shunt (Ioffe): 14mm; 18 turns; 
+% % pitch=500um (wire dia); AH sep ~17mm; Q-Sh sep=18.5 mm; ~36 amp (max)
+% Dquad=14e-3;
+% Dshunt=14e-3;
+% 
+% Nturnshunt=18;
+% Nturnquad=10;
+% pitch_coil=0.5e-3;
+% 
+% disp_ah=17e-3;
+% disp_qs=18.5e-3;
+% 
+% Iquad=1;
+% Ishunt=1.5;
+% 
+% Rquad=Dquad/2;
+% Rshunt=Dshunt/2;
+% 
+% % Trap bias (nuller) [http://dx.doi.org/10.1063/1.2472600]
+% Bbias=1e-4*[1,0,0];     % external bias field [T] (uniform assumption)
+% 
+% %%% Build trap
+% % Quadrupole - ref
+% quad_coil.type='coil';
+% quad_coil.param={Rquad,Iquad,[0,0,disp_ah/2],[0,0,0]};
+% % Shunt (Ioffe) - ref
+% shunt_coil.type='coil';
+% shunt_coil.param={Rshunt,Ishunt,[disp_qs,0,disp_ah/2],[0,0,0]};
+% % Bias (nuller)
+% bias.type='uniform';
+% bias.param={Bbias};
+% 
+% btrap=[];
+% for ii=1:Nturnquad
+%     quad_coil_temp=quad_coil;
+%     % shift by wire pitch
+%     quad_coil_temp.param{3}=quad_coil_temp.param{3}+(ii-1)*[0,0,pitch_coil];
+%     btrap=[btrap,quad_coil_temp];
+%     
+%     % anti-helmholtz pair - mirror symmetry around Z
+%     quad_coil_temp.param{2}=-1*quad_coil_temp.param{2};     % flip current dir
+%     quad_coil_temp.param{3}=[1,1,-1].*quad_coil_temp.param{3};
+%     btrap=[btrap,quad_coil_temp];
+% end
+% for ii=1:Nturnshunt
+%     shunt_coil_temp=shunt_coil;
+%     % shift by wire pitch
+%     shunt_coil_temp.param{3}=shunt_coil_temp.param{3}+(ii-1)*[0,0,pitch_coil];
+%     btrap=[btrap,shunt_coil_temp];
+%     
+%     % anti-helmholtz pair - mirror symmetry around Z
+%     shunt_coil_temp.param{2}=-1*shunt_coil_temp.param{2};   % flip current dir
+%     shunt_coil_temp.param{3}=[1,1,-1].*shunt_coil_temp.param{3};
+%     btrap=[btrap,shunt_coil_temp];
+% end
+% btrap=[btrap,bias];
 
 %%% Trap magnetic field calculation
 [Bmag,Bxyz]=trap_eval(btrap,XYZ{:});
